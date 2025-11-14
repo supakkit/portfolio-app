7eb64e3f4fbe67a0ea052854d098f4173ee39ab9
supakkit <87434100+supakkit@users.noreply.github.com>
2025-11-14 17:16:10 +0700

    feat(theme): change style of theme toggle

--- src/components/navigation/theme-toggle.tsx
+++ src/components/navigation/theme-toggle.tsx
@@ -1,7 +1,81 @@
-import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
+"use client";
 
-export function ThemeToggle() {
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Moon, Sun } from "lucide-react";
+import { flushSync } from "react-dom";
+import { Button } from "../ui/button";
+
+export const ThemeToggle = () => {
+  const [isDark, setIsDark] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const duration = 400;
+
+  useEffect(() => {
+    const updateTheme = () => {
+      setIsDark(document.documentElement.classList.contains("dark"));
+    };
+
+    updateTheme();
+
+    const observer = new MutationObserver(updateTheme);
+    observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ["class"],
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
+  const toggleTheme = useCallback(async () => {
+    if (!buttonRef.current) return;
+
+    await document.startViewTransition(() => {
+      flushSync(() => {
+        const newTheme = !isDark;
+        setIsDark(newTheme);
+        document.documentElement.classList.toggle("dark");
+        localStorage.setItem("theme", newTheme ? "dark" : "light");
+      });
+    }).ready;
+
+    const { top, left, width, height } =
+      buttonRef.current.getBoundingClientRect();
+    const x = left + width / 2;
+    const y = top + height / 2;
+    const maxRadius = Math.hypot(
+      Math.max(left, window.innerWidth - left),
+      Math.max(top, window.innerHeight - top)
+    );
+
+    document.documentElement.animate(
+      {
+        clipPath: [
+          `circle(0px at ${x}px ${y}px)`,
+          `circle(${maxRadius}px at ${x}px ${y}px)`,
+        ],
+      },
+      {
+        duration,
+        easing: "ease-in-out",
+        pseudoElement: "::view-transition-new(root)",
+      }
+    );
+  }, [isDark, duration]);
+
   return (
-    <AnimatedThemeToggler className="cursor-pointer" title="Toggle theme" />
+    <Button
+      variant="outline"
+      size="icon"
+      className="rounded-full cursor-pointer"
+      ref={buttonRef}
+      onClick={toggleTheme}
+    >
+      {isDark ? (
+        <Sun className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   );
-}
+};
