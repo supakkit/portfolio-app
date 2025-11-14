aefb15756603a7e58a0a44916c26b4d5d6a86ad0
supakkit <87434100+supakkit@users.noreply.github.com>
2025-11-14 18:11:17 +0700

    refactor(theme): refactor theme toggle component

--- src/components/navigation/theme-toggle.tsx
+++ src/components/navigation/theme-toggle.tsx
@@ -1,47 +1,35 @@
 "use client";
 
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useRef } from "react";
 import { Moon, Sun } from "lucide-react";
 import { flushSync } from "react-dom";
 import { Button } from "../ui/button";
+import { useTheme } from "next-themes";
 
 export const ThemeToggle = () => {
-  const [isDark, setIsDark] = useState(false);
+  const { resolvedTheme, setTheme } = useTheme();
+  const isDark = resolvedTheme === "dark";
   const buttonRef = useRef<HTMLButtonElement>(null);
   const duration = 400;
-
-  useEffect(() => {
-    const updateTheme = () => {
-      setIsDark(document.documentElement.classList.contains("dark"));
-    };
-
-    updateTheme();
-
-    const observer = new MutationObserver(updateTheme);
-    observer.observe(document.documentElement, {
-      attributes: true,
-      attributeFilter: ["class"],
-    });
-
-    return () => observer.disconnect();
-  }, []);
 
   const toggleTheme = useCallback(async () => {
     if (!buttonRef.current) return;
 
+    const newTheme = isDark ? "light" : "dark";
+
     await document.startViewTransition(() => {
       flushSync(() => {
-        const newTheme = !isDark;
-        setIsDark(newTheme);
-        document.documentElement.classList.toggle("dark");
-        localStorage.setItem("theme", newTheme ? "dark" : "light");
+        setTheme(newTheme);
       });
     }).ready;
 
+    // Animation
     const { top, left, width, height } =
       buttonRef.current.getBoundingClientRect();
+
     const x = left + width / 2;
     const y = top + height / 2;
+
     const maxRadius = Math.hypot(
       Math.max(left, window.innerWidth - left),
       Math.max(top, window.innerHeight - top)
@@ -60,7 +48,7 @@ export const ThemeToggle = () => {
         pseudoElement: "::view-transition-new(root)",
       }
     );
-  }, [isDark, duration]);
+  }, [isDark, setTheme]);
 
   return (
     <Button
@@ -71,11 +59,12 @@ export const ThemeToggle = () => {
       onClick={toggleTheme}
     >
       {isDark ? (
-        <Sun className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+        <Sun className="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
       ) : (
-        <Moon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+        <Moon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
       )}
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
 };
+
